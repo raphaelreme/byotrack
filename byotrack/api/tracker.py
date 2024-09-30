353032b58450a77eae379aff638461d644bcf042
Raphael Reme <raphaelreme@protonmail.com>
2024-09-30 19:00:14 +0200

    Measure correctly time in the OnlineBatchTracker
    cf https://github.com/tqdm/tqdm/pull/1617

--- byotrack/api/tracker.py
+++ byotrack/api/tracker.py
@@ -2,12 +2,59 @@ from __future__ import annotations
 
 from abc import ABC, abstractmethod
 from typing import Collection, Iterable, List, Sequence, Union
+import warnings
 
 import numpy as np
 import tqdm.auto as tqdm
 
 import byotrack  # pylint: disable=cyclic-import
 from .parameters import ParametrizedObjectMixin
+
+
+class PauseableTQDM(tqdm.tqdm):  # pylint: disable=inconsistent-mro
+    """Tqdm with a real pause mechanism"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+
+        self.last_pause_t = 0.0
+
+    def update(self, n: float | None = 1) -> bool | None:
+        if self.last_pause_t != 0.0:
+            warnings.warn("Progress bar is paused. You should explicitly call unpause before updating again")
+            self.unpause()
+
+        return super().update(n)
+
+    def pause(self, refresh=True):
+        if self.disable:
+            return
+
+        if self.last_pause_t != 0.0:
+            warnings.warn("The progress bar was already paused")
+            return
+
+        if refresh:  # By default refresh before doing a pause
+            self.refresh()
+
+        self.last_pause_t = self._time()  # type: ignore
+
+    def unpause(self):
+        if self.disable:
+            return
+
+        if self.last_pause_t == 0.0:
+            warnings.warn("The progress bar was not paused")
+            return
+
+        delta_t = self._time() - self.last_pause_t  # type: ignore
+        self.last_pause_t = 0.0
+        if delta_t < 0.0:
+            warnings.warn("Found negative pause time, ignoring...")
+            return
+
+        self.start_t += delta_t
+        self.last_print_t += delta_t
 
 
 class Tracker(ABC, ParametrizedObjectMixin):  # pylint: disable=too-few-public-methods
@@ -99,8 +146,9 @@ class BatchMultiStepTracker(MultiStepTracker):  # pylint: disable=too-few-public
         detections_sequence: Sequence[byotrack.Detections] = []
         frame_ids: List[int] = []
 
-        detect_bar = tqdm.tqdm(desc=self.detector.progress_bar_description, total=len(video))
-        link_bar = tqdm.tqdm(desc=self.linker.progress_bar_description, total=len(video))
+        detect_bar = PauseableTQDM(desc=self.detector.progress_bar_description, total=len(video))
+        link_bar = PauseableTQDM(desc=self.linker.progress_bar_description, total=len(video))
+        link_bar.pause()  # Starting with detections
 
         first = video[0]
         batch = np.zeros((self.detector.batch_size, *first.shape), dtype=first.dtype)
@@ -113,12 +161,18 @@ class BatchMultiStepTracker(MultiStepTracker):  # pylint: disable=too-few-public
                 detections_sequence = self.detector.detect(batch)
                 detect_bar.update(n)
 
+                # Switch from detection to linking
+                detect_bar.pause()
+                link_bar.unpause()
+
                 for i in range(n):
                     detections_sequence[i].frame_id = frame_ids[i]
                     self.linker.update(batch[i], detections_sequence[i])
                     link_bar.update()
 
-                link_bar.refresh()
+                # Switch from linking to detection
+                link_bar.pause()
+                detect_bar.unpause()
 
                 frame_ids = []
                 n = 0
@@ -129,13 +183,15 @@ class BatchMultiStepTracker(MultiStepTracker):  # pylint: disable=too-few-public
 
         detections_sequence = self.detector.detect(batch[:n])
         detect_bar.update(n)
+        detect_bar.close()
+
+        link_bar.unpause()
 
         for i in range(n):
             detections_sequence[i].frame_id = frame_ids[i]
             self.linker.update(batch[i], detections_sequence[i])
             link_bar.update()
 
-        detect_bar.close()
         link_bar.close()
 
         tracks = self.linker.collect()
