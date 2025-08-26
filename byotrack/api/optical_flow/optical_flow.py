Better construction of initial covariance + Small fixes
Use a fully defined initial covariance for all Kalman filters.

Fixes:
- Anisotropy is deprecated in RTSSmotther
- typing in optical_flow api

byotrack/api/optical_flow/optical_flow.py
@@ -180,7 +180,7 @@ class OpticalFlow(ABC):
 
         """
         *dims, channels = moving.shape
-        points = np.indices(dims, np.float32).reshape(len(dims), -1).transpose()
+        points: np.ndarray = np.indices(dims, np.float32).reshape(len(dims), -1).transpose()
         points = self.transform(flow_map, points).transpose().reshape(len(dims), *dims)
 
         return np.concatenate(

byotrack/implementation/linker/frame_by_frame/kalman_linker.py
@@ -115,10 +115,11 @@ class KalmanLinkerParameters(FrameByFrameLinkerParameters):
             or LIKELIHOOD `cost_method`. As this may be detrimental, it is disabed by default.
             Default: 0.0 (Process_std is constant)
         initial_std_factor (float): The uncertainties on initial velocities/accelerations are set
-            to initial_std_factor * process_std. Having a small factor will prevent handling correctly
-            starting tracks that already moves on their first frames. But large values will lead to large uncertainty
-            on the first prediction, making it hard to associate to a detection with MAHALANOBIS
-            or LIKELIHOOD methods. Typical values lies in 3.0 to 10.0.
+            to initial_std_factor * process_std. See `KalmanLinker.build_initial_covariance`.
+            Having a small factor will prevent handling correctly starting tracks with large initial velocity
+            on their first frames. But large values will lead to large uncertainty on the first prediction, making
+            it hard to associate to a detection with MAHALANOBIS or LIKELIHOOD methods.
+            Typical values lies between 3.0 to 10.0.
             Default: 10.0
 
     """
@@ -404,18 +405,13 @@ class KalmanLinker(FrameByFrameLinker):
         # Infinite uncertainty on the prior position => Given positional measurement, we initialize on its position
         #                                               with the positional measurement uncertainty.
         # For derivatives, we assume they are centered on 0 (no bias toward a direction) with an uncertainty of
-        # initial_std * process_std:
+        # initial_std_factor * process_std:
         # Small factors (~1) are not suited for tracking objects that appears with a large initial velocity.
         # Large factors (>> 1) will create association problem on the second frame, as the predicted
         # position inherit from this uncertainty.
         initial_state = torch_kf.GaussianState(
             torch.zeros(len(unmatched_measures), self.kalman_filter.state_dim, 1, dtype=self.dtype),
-            (
-                torch.eye(self.kalman_filter.state_dim, dtype=self.dtype)
-                * (self.specs.initial_std_factor * self.specs.process_std) ** 2
-            )[None]
-            .expand(len(unmatched_measures), self.kalman_filter.state_dim, self.kalman_filter.state_dim)
-            .clone(),
+            torch.stack([self.build_initial_covariance(detections.dim)] * len(unmatched_measures)),
         )
         initial_state.mean[:, : detections.dim, 0] = unmatched_measures
         initial_state.covariance[:, : detections.dim, : detections.dim] = self.kalman_filter.measurement_noise[
@@ -455,3 +451,35 @@ class KalmanLinker(FrameByFrameLinker):
 
         if self.save_all or self.specs.track_building == TrackBuilding.SMOOTHED:
             self.all_states.append(self.active_states)
+
+    def build_initial_covariance(self, dim: int) -> torch.Tensor:
+        """Build the diagonal initial covariance matrix
+
+        The position is initially unknown, leading to a belief (given by the first detection) set
+        to the position of the first detection, with detection_std uncertainty.
+
+        The velocity (and higher order derivatives) are assumed to be 0.0 with a relatively high uncertainty:
+        initial_std_factor * process_std.
+
+        Note that having a large initial_std_factor (>10) may decrease performances, as the first prediction
+        will be impacted and largely uncertain, leading to low probabilies for every associations. In KOFT,
+        as the velocity is measured before the first prediction, the initial_std_factor can be increased to
+        reduce this bias toward a nul initial velocity. We found that initial_std_factor=0.0 is a good
+        trade off in practice.
+        """
+        process_std = torch.broadcast_to(torch.as_tensor(self.specs.process_std, dtype=self.dtype), (dim,)).clone()
+
+        # In the case of Brownian motion, the intitial covariance if fully rewritten in SKT (no impact)
+        # But in KOFT, Brownian motion models velocity with an initial velocity centered on 0 and with
+        # an uncertainty given by the process_std, therefore we don't use initial_std_factor
+        if self.specs.kalman_order > 0:
+            process_std *= self.specs.initial_std_factor
+
+        # Process std is squared and set for each order of the process (pos, vel, acc, ...)
+        covariance = torch.diag(torch.cat([process_std**2] * (self.kalman_filter.state_dim // dim)))
+
+        # Then, it is overwritten for the position, to be set to measurement_std
+        measurement_std = torch.broadcast_to(torch.as_tensor(self.specs.detection_std, dtype=self.dtype), (dim,))
+        torch.diagonal(covariance)[:dim] = measurement_std**2
+
+        return covariance

byotrack/implementation/linker/frame_by_frame/koft.py
@@ -79,10 +79,10 @@ class KOFTLinkerParameters(KalmanLinkerParameters):
             or LIKELIHOOD `cost_method`. As this may be detrimental, it is disabed by default.
             Default: 0.0 (Process_std is constant)
         initial_std_factor (float): The uncertainties on initial velocities/accelerations are set
-            to initial_std_factor * process_std. Having a small factor will prevent handling correctly
-            starting tracks that already moves on their first frames. But large values will lead to large uncertainty
-            on the first prediction, making it hard to associate to a detection with MAHALANOBIS
-            or LIKELIHOOD methods. Typical values lies in 3.0 to 10.0.
+            to initial_std_factor * process_std. See `KalmanLinker.build_initial_covariance`.
+            Having a small factor will prevent handling correctly starting tracks with large initial velocity
+            on their first frames.
+            Typical values lies between 3.0 to 100.0.
             Default: 10.0
 
     """
@@ -346,20 +346,15 @@ class KOFTLinker(KalmanLinker):
         # Infinite uncertainty on the prior position => Given positional measurement, we initialize on its position
         #                                               with the positional measurement uncertainty.
         # For derivatives, we assume they are centered on 0 (no bias toward a direction) with an uncertainty of
-        # initial_std * process_std:
+        # initial_std_factor * process_std:
         # Small factors (~1) are not suited for tracking objects that appears with a large initial velocity.
         # Large factors (>> 1) will create association problem on the second frame, as the predicted
         # position inherit from this uncertainty.
         # As in KOFT, the velocities are measured, it could be set to a higher value.
         # For the Brownian motion, the process_std directly gives the initial uncertainty on velocity
-        std_factor = self.specs.initial_std_factor if self.specs.kalman_order != 0 else 1.0
         initial_state = torch_kf.GaussianState(
             torch.zeros(len(unmatched_measures), self.kalman_filter.state_dim, 1, dtype=self.dtype),
-            (torch.eye(self.kalman_filter.state_dim, dtype=self.dtype) * (std_factor * self.specs.process_std) ** 2)[
-                None
-            ]
-            .expand(len(unmatched_measures), self.kalman_filter.state_dim, self.kalman_filter.state_dim)
-            .clone(),
+            torch.stack([self.build_initial_covariance(detections.dim)] * len(unmatched_measures)),
         )
         initial_state.mean[:, : detections.dim, 0] = unmatched_measures
         initial_state.covariance[:, : detections.dim, : detections.dim] = self.kalman_filter.measurement_noise[

byotrack/implementation/refiner/smoother.py
@@ -11,6 +11,7 @@ import byotrack
 
 
 # TODO: Add a KOFTSmoother ? that uses optical flow to smooth tracks as in KOFT
+# TODO: Support for float64 and cuda ?
 
 
 class RTSSmoother(byotrack.Refiner):  # pylint: disable=too-few-public-methods
@@ -41,10 +42,12 @@ class RTSSmoother(byotrack.Refiner):  # pylint: disable=too-few-public-methods
         kalman_order (int): Order of the Kalman filter to use.
             0 for brownian motions, 1 for directed brownian motion, 2 for accelerated brownian motions, etc...
             Default: 1
-        anisotropy (Tuple[float, float, float]): Anisotropy of images (Ratio of the pixel sizes
-            for each axis, depth first). This will be used to scale distances.
-            More precisely, given std(s) are divided by the anisotropy.
-            Default: (1., 1., 1.)
+        anisotropy (Tuple[float, float, float]): Deprecated and ignored. If the data is anisotrope, it should directly
+            be incorporated inside detection_std and process_std. Will be removed in a future version.
+        initial_std_factor (float): The initial state is created with an initial uncertainty
+            set to the measurement_std of the Kalman Filter on measured states and this factor times
+            the process_std for unmeasured states.
+            Default: 10.0
 
     """
 
@@ -56,13 +59,22 @@ class RTSSmoother(byotrack.Refiner):  # pylint: disable=too-few-public-methods
         detection_std: Union[float, torch.Tensor] = 3.0,
         process_std: Union[float, torch.Tensor] = 1.5,
         kalman_order: int = 1,
+        *,
         anisotropy: Tuple[float, float, float] = (1.0, 1.0, 1.0),
+        initial_std_factor=10.0,
     ) -> None:
         super().__init__()
         self.detection_std = detection_std
         self.process_std = process_std
         self.kalman_order = kalman_order
-        self.anisotropy = anisotropy
+        self.initial_std_factor = initial_std_factor
+
+        if anisotropy != (1.0, 1.0, 1.0):
+            warnings.warn(
+                "'anisotropy' is deprecated and will be removed in a future version; it is ignored.",
+                DeprecationWarning,
+                stacklevel=2,
+            )
 
     def run(  # pylint: disable=too-many-locals
         self, _: Union[Sequence[np.ndarray], np.ndarray], tracks: Collection[byotrack.Track]
@@ -72,11 +84,9 @@ class RTSSmoother(byotrack.Refiner):  # pylint: disable=too-few-public-methods
 
         dim = next(iter(tracks)).points.shape[-1]
 
-        anisotropy = torch.tensor(self.anisotropy)[-dim:]
-
         kalman_filter = torch_kf.ckf.constant_kalman_filter(
-            self.detection_std / anisotropy,
-            self.process_std / anisotropy,
+            self.detection_std,
+            self.process_std,
             dim=dim,
             order=self.kalman_order,
         )
@@ -93,13 +103,17 @@ class RTSSmoother(byotrack.Refiner):  # pylint: disable=too-few-public-methods
             is_defined[track.start + offset : track.start + len(track), i] = True
             offsets.append(offset)
 
-        # Filtering
+        # Initial state (see KalmanLinker)
+        process_std = torch.broadcast_to(torch.as_tensor(self.process_std), (dim,)) * self.initial_std_factor
+        process_std = torch.cat([process_std**2] * (kalman_filter.state_dim // dim))  # Expand to state_dim
+
         initial_state = torch_kf.GaussianState(
             torch.zeros(1, kalman_filter.state_dim, 1),
-            kalman_filter.process_noise[None] * torch.eye(kalman_filter.state_dim) * 10,
+            torch.diag(process_std)[None],
         )
         initial_state.covariance[:, :dim, :dim] = kalman_filter.measurement_noise
 
+        # Filtering
         states = torch_kf.GaussianState(
             torch.zeros(positions.shape[0], positions.shape[1], kalman_filter.state_dim, 1),
             torch.zeros(positions.shape[0], positions.shape[1], kalman_filter.state_dim, kalman_filter.state_dim),